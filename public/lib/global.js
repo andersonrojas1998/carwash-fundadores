efe24a241481995e187f6068e74c3434410f9299
AndersonR <rojasanderson07@gmail.com>
2025-08-19 00:39:10 -0500

    change password

--- public/lib/global.js
+++ public/lib/global.js
@@ -1,12 +1,8 @@
 $(document).ready(function() {
 
-
-
   $(document).on('click', '#btn_change_password', function() {
     window.location.href = '/change-password';
   });
-
-
   if(window.innerWidth < 768){
 
     if($('.btn-w-all').length){
